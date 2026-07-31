1ecad16f261fd3147f28ccc6c37d0aabf5bfd710
preeti12345678gupta-hue <preeti12345678gupta@gmail.com>
2026-08-01 05:07:59 +0530

    Add: remove-element - submission-0

---
+++ Data Structures & Algorithms/remove-element/submission-0.cs
@@ -0,0 +1,13 @@
+public class Solution {
+    public int RemoveElement(int[] nums, int val) {
+        int write = 0;
+        for(int i = 0; i < nums.Length; i++){
+            if(nums[i] != val){
+                nums[write] = nums[i];
+                write++;
+            }
+        }
+
+        return write;
+    }
+}
