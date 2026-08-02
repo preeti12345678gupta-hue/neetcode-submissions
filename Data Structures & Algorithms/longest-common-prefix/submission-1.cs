4b06f38976f759d618b520236b1964755b3ec656
preeti12345678gupta-hue <preeti12345678gupta@gmail.com>
2026-08-02 06:06:57 +0530

    Add: longest-common-prefix - submission-1

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-1.cs
@@ -0,0 +1,21 @@
+public class Solution {
+    public string LongestCommonPrefix(string[] strs) {
+        if(strs.Length == 0){
+            return "";
+        }
+        string prefix = strs[0];
+        for(int i = 1; i < strs.Length; i++){
+            
+
+            while(!strs[i].StartsWith(prefix)){
+                prefix = prefix.Substring(0,prefix.Length - 1);
+                if(prefix == ""){
+                    return "";
+                }
+
+            }
+        }
+
+        return prefix;
+    }
+}
