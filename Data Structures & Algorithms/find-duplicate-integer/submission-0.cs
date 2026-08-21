053c5ee16e1cf7c2df8f39d430f22d345c76d1ce
preeti12345678gupta-hue <preeti12345678gupta@gmail.com>
2026-08-22 04:21:16 +0530

    Add: find-duplicate-integer - submission-0

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-0.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public int FindDuplicate(int[] nums) {
+        HashSet<int> map = new HashSet<int>();
+        foreach(int num in nums){
+            if(map.Contains(num)){
+                return num;
+            }
+            map.Add(num);
+        }
+        return -1;
+    }
+}
